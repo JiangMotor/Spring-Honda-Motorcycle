add706c13131ff122f75351c766d7359c45cffa6
JiangMotor <51698969+JiangMotor@users.noreply.github.com>
2019-07-19 11:27:22 +0700

    Update Honda.java

--- src/main/java/com/example/jianghonda/model/honda/Honda.java
+++ src/main/java/com/example/jianghonda/model/honda/Honda.java
@@ -15,7 +15,7 @@ public class Honda{
     private String img_honda;
     private String name_honda;
     private String video_honda;
-    private String category;
+    private String category_name;
 
 
 }
